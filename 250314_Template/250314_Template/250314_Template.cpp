63c348eb41688452c0adb5c4311419507fd4b41c
mi1128h <70762557+mi1128h@users.noreply.github.com>
2025-03-14 10:13:44 +0900

    Lecture: template

---
+++ 250314_Template/250314_Template/250314_Template.cpp
@@ -0,0 +1,42 @@
+﻿// 250314_Template.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+/*
+    템플릿: 다양한 자료형에 대해 일반화된 코드(Generic)를 작성할 수 있게 하는 문법
+*/
+
+// 함수 템플릿
+template<typename T>
+T Add(T a, T b) { return a + b; }
+
+// 팩토리얼
+template<int N>
+struct Factorial {
+    static const int value = N * Factorial<N - 1>::value;
+};
+
+template<>
+struct Factorial<0> {
+    static const int value = 1;
+};
+
+int main()
+{
+    std::cout << Factorial<5>::value << std::endl;
+    std::cout << Add(5, 3) << std::endl;
+    std::cout << Add(5.7f, 3.9f) << std::endl;
+    std::cout << (int)Add('a', 'c') << std::endl;
+}
+
+// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
+// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+
+// 시작을 위한 팁: 
+//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
+//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
+//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
+//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
+//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
+//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
